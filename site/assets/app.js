7a93a389feccb868b8c18cd8afdd3db5e584b846
Trinh Luong Huu Duc <huu-duc.trinh-luong@ekino.com>
2021-08-11 23:58:14 +0700

    fix(front-end): Remove unused package

--- site/assets/app.js
+++ site/assets/app.js
@@ -8,9 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/main.scss'
 
-// start the Stimulus application
-import './bootstrap';
-
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
